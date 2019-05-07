4b2b22be03b540a6dce8eab26967be2602f869d4
Dilraj Singh <dilrajsingh.7@gmail.com>
2019-05-07 08:11:52 -0700

    wip

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
+import { unstable_createResource } from 'react-cache';
+
+const restaurantListResource = unstable_createResource(() => {
+  return new Promise((resolve, reject) => {
+    fetch('https://jsonplaceholder.typicode.com/todos/')
+      .then(res => res.json())
+      .then(x => new Promise(resolve => setTimeout(() => resolve(x), 2000)))
+      .then(response => {
+        resolve(response);
+      });
+  });
+});
+
+const RestaurantList = () => {
+  // if no restaurants are found in the cache, the suspense will throw a promise
+  const restaurants = restaurantListResource.read();
+  // this line will have to wait until that promise resolves
+  return (
+    <article>
+      <h2 className="f3 fw4 pa3 mv0">Restaurant List</h2>
+      <div className="cf pa2">{JSON.stringify(restaurants)}</div>
+    </article>
+  );
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <h2 style={{ textAlign: "center" }}>{`React: ${React.version} Demo`}</h2>
+
+      <React.Suspense delayMs={50} fallback={'Loading...'}>
+        <RestaurantList />
+      </React.Suspense>
+    </React.Fragment>
   );
 }
 
